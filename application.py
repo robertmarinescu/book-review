493f891d4d6a9ba26ee0484d96a28d59c87e68f0
Robert Marinescu <grmarinescu@axway.com>
2020-04-29 17:54:31 +0300

    Clarify layout template and customize login and register page

--- application.py
+++ application.py
@@ -1,10 +1,11 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+from helpers import *
 
 app = Flask(__name__)
 
@@ -22,7 +23,18 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+
 @app.route("/")
+@login_required
 def index():
     return "Project 1: TODO"
 
