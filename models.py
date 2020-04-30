203855c3c952f79a285309c6aa22971d0affeb7a
Robert Marinescu <robert.marinescu93@gmail.com>
2020-04-30 17:12:03 +0300

    Add error handling for register.html

---
+++ models.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
