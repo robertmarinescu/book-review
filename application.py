5cbd65ece4ce522b05303ff3f51fbcfaa23474c0
Robert Marinescu <robert.marinescu93@gmail.com>
2020-05-01 12:21:13 +0300

    Implement register functionality

--- application.py
+++ application.py
@@ -5,6 +5,7 @@ from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from helpers import *
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 
@@ -31,12 +32,12 @@ def register():
             return render_template("error.html", message="you must provide an username")
 
         # Query database for username
-        # userCheck = db.execute("SELECT * FROM users WHERE username = :username",
-        #                        {"username": request.form.get("username")}).fetchone()
+        userCheck = db.execute("SELECT * FROM users WHERE username = :username",
+                               {"username": request.form.get("username")}).fetchone()
 
         # Check if username already exist
-        # if userCheck:
-        #     return render_template("error.html", message="username already exist")
+        if userCheck:
+            return render_template("error.html", message="username already exist")
 
         # Ensure password was submitted
         elif not request.form.get("password"):
@@ -49,6 +50,14 @@ def register():
         # Check passwords are equal
         elif not request.form.get("password") == request.form.get("confirmation"):
             return render_template("error.html", message="passwords didn't match")
+
+        hash_password = generate_password_hash(request.form.get("password"))
+        db.execute("INSERT INTO users (username, password) VALUES(:username, :password)",
+                   {"username": request.form.get("username"),
+                    "password": hash_password})
+
+        db.commit()
+        flash('Account successfully created')
 
         # Redirect user to login page
         return redirect("/login")
